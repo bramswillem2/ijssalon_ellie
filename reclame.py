7e15f1d0efce871f9b30219c775b6b4ffc1e5d78
Bram van de Sande <b.f.l.a.vdsande@tilburguniversity.edu>
2025-04-14 15:43:26 +0200

    huiswerkupdate

--- reclame.py
+++ reclame.py
@@ -35,8 +35,9 @@ def meervoudig(invoer_lijst):
     return invoer_lijst
 print(meervoudig(lijst_hoog_laag))
 
+
 def combinatie(invoer_lijst_2):
     korte_lijst = laag_en_hoog(invoer_lijst_2)
     uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
     return uitvoer
-print(combinatie(mijn_functie_2))
+print(combinatie(100))
