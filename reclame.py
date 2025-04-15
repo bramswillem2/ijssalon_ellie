96f63cc70678b1485842e3aa391c3322e8724b3b
Bram van de Sande <b.f.l.a.vdsande@tilburguniversity.edu>
2025-04-15 08:57:02 +0200

    aangepast na huiswerk

--- reclame.py
+++ reclame.py
@@ -10,7 +10,7 @@ lijst_inkomsten = [220, 430, 125, 160, 205, 90, 345]
 def inkomsten_totaal(inkomsten, btw):
     global lijst_inkomsten
     inkomsten = sum(lijst_inkomsten)
-    btw_bedrag = inkomsten * btw
+    btw_bedrag = float(inkomsten * btw)
     uitvoer = f"Het totaal van alle inkomsten van deze week is {inkomsten}, waarover {btw_bedrag} euro btw betaald dient te worden"
     return uitvoer
 print(inkomsten_totaal(lijst_inkomsten, 0.09))
@@ -28,16 +28,15 @@ def gemiddelde(mijn_lijst):
     return uitvoer
 print(gemiddelde(lijst_inkomsten))
 
-lijst_hoog_laag = [10,5,3,2,1,2,9]
 def meervoudig(invoer_lijst):
-    global lijst_hoog_laag
-    invoer_lijst = laag_en_hoog(lijst_hoog_laag)
-    return invoer_lijst
-print(meervoudig(lijst_hoog_laag))
-
+    tijdelijk = laag_en_hoog(invoer_lijst)
+    uitvoer = [tijdelijk[0],tijdelijk[1]]
+    return uitvoer
+print(meervoudig([100,10,20,50]))
 
 def combinatie(invoer_lijst_2):
     korte_lijst = laag_en_hoog(invoer_lijst_2)
     uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
     return uitvoer
-print(combinatie(100))
+print(combinatie([ 100,150,200,1000,1250]))
+
--- test.py
+++ test.py
@@ -6,3 +6,27 @@ def mijn_functie(a,b):
     else:
         return "niets"
 print (mijn_functie(12,2))
+
+def inkomsten_totaal(inkomsten, btw):
+    totaal = 0
+    for bedrag in inkomsten:
+        totaal += bedrag
+        btw_bedrag = totaal * btw
+        uitvoer = f"het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw_bedrag} euro btw betaald dient te worden"
+        return uitvoer
+print(inkomsten_totaal([200,100,300],0.21))
+
+def laag_en_hoog(mijn_lijst):
+    uitvoer = []
+    laagste = min(mijn_lijst)
+    hoogste = max(mijn_lijst)
+    uitvoer.append(laagste)
+    uitvoer.append(hoogste)
+    return uitvoer
+print(laag_en_hoog([100,20,10]))
+
+def meervoudig(invoer_lijst):
+    tijdelijk = laag_en_hoog(invoer_lijst)
+    uitvoer = [tijdelijk[0],tijdelijk[1]]
+    return uitvoer
+print(meervoudig([150,5,20,50]))
